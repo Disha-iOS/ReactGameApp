73edb538ed4c8f14004f5cc2cca6ed47bfadba0e
Disha Shekhat <disha.shekhat@tatvasoft.com>
2020-07-21 09:54:36 +0530

    learn to use Platform API of react

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Platform} from 'react-native';
 import Colors from '../constants/colors';
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View style={{...styles.headerBase,...Platform.select({ios: styles.headerIos, android: styles.headerAndroid})}}>
       <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 88,
     paddingTop: 36,
-    backgroundColor: Colors.accent,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerIos: {
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+  },
+  headerAndroid: {
+    backgroundColor: Colors.accent ,
   },
   headerTitle: {
     color: 'black',
